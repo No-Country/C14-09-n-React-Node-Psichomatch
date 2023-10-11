46e904a9d5fb9f7bbd0a4b854720b62c30a8c809
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-10-11 10:33:08 -0400

    creacion de login y registro maquetado

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Login from './Components/Login'
 
 function App() {
   return (
     <>
-      <div>
-       <h1 className="text-3xl font-bold text-center mt-20">Sistema de Citas de Terapia Psicologica</h1>
-      </div>
+     <Login />
     </>
   )
 }
