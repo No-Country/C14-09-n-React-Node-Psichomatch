ba6c547018b72efcb9271573404d34934bfefc47
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-10-11 17:50:01 -0400

    cambio nombre carpeta

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './Components/Login'
+import Registro from './Components/Registro'
 
 function App() {
   return (
     <>
      <Login />
+     <Registro />
     </>
   )
 }
