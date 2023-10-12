fff60eeb234ee1cba60b831cd120def92c1ca773
scarletvargas <scarletvf47@gmail.com>
2023-10-12 17:34:58 -0400

    Creacion de la seccion hero - Scarlet

---
+++ Frontend/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import imgHero from "../assets/img-hero.png";
+
+export const Hero = () => {
+  return (
+    <div className="hero flex gap-4 flex-col my-6 mx-4 lg:flex-row lg:justify-between lg:my-12 lg:mx-20">
+      <div className="content max-w-lg flex justify-between flex-col gap-6 lg:flex-1">
+        <h1 className="text-[#383838] text-5xl font-semibold">
+          Encuentra apoyo psicológico con facilidad, en cualquier momento y
+          lugar.
+        </h1>
+        <p className="text-black text-2xl mb-5">Cultiva tu bienestar emocional</p>
+        <button className="text-[#383838] text-2xl font-semibold bg-[#B0A0DF] py-4 px-6 rounded-[32px] w-[384px]">
+          Agenda una cita
+        </button>
+      </div>
+      <div className="img max-w-xl flex justify-center items-center lg:flex-1">
+        <img
+          src={imgHero}
+          alt="Relaxed Woman"
+          className="bg-contain bg-no-repeat"
+        />
+      </div>
+    </div>
+  );
+};
+
