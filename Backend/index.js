c35710a4313257c783b4da8645fb19d7a4e07a93
FranprzDev <franutn23@gmail.com>
2023-10-12 22:19:36 -0300

    Therapist functions and common fill

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const server = require("./src/app.js");
-const { conn, User, Contact} = require("./src/db.js");
-const {fillUser} = require("./src/controllers/user.js")
-const {fillContact} = require("./src/controllers/Contact.js")
+const { conn, User, Contact, Therapist } = require("./src/db.js");
+const { fillUser } = require("./src/controllers/user.js")
+const { fillContact } = require("./src/controllers/Contact.js")
+const { fillTherapist } = require("./src/controllers/therapist.js");
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     fillUser(User);
     fillContact(Contact);
+    // Fill Therapist -->
+    fillTherapist(Therapist);
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     //fillTemperaments(Temperament);
   });
