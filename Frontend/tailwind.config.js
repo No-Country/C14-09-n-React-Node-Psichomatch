49e8fc05a9cb193f22df6adeafa04952b6ef9999
scarletvargas <scarletvf47@gmail.com>
2023-10-14 16:22:18 -0400

    Add colors

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'purple':  '#B0A0DF',
+      'purple-ligth':  '#DFD6FB',
+      'green': '#BEDEBA',
+      'gray-dark': '#383838',
+      'gray': '#E9E9E9',
+    },
     extend: {},
   },
   plugins: [],
