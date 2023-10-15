ab7946f04b22952864a3cb7d84a52f4b7033d7b8
erasmorojastech <erasmorojastech@gmail.com>
2023-10-15 17:44:43 -0300

    Add Layouts to the project

---
+++ Frontend/src/Layouts/Dashboard-Agenda.jsx
@@ -0,0 +1,22 @@
+import TherapistInfo from "../Components/Therapist-Info"
+
+const Dashboard_Agenda = function () {
+  return (
+    <div className="bg-teal-100 p-5 row-start-2 row-end-3 col-start-2 col-end-6 flex flex-col items-center justify-start gap-5">
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+      <TherapistInfo />
+    </div>
+  )
+}
+
+export default Dashboard_Agenda;
