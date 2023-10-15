e915f88deee4da12c36bf8e22c35b5624781583d
erasmorojastech <erasmorojastech@gmail.com>
2023-10-15 17:43:52 -0300

    Add Components to the project

---
+++ Frontend/src/Components/Therapist-Info.jsx
@@ -0,0 +1,12 @@
+const TherapistInfo = function () {
+  return (
+    <div className="w-[65%] bg-blue-400 p-5 rounded-lg">
+      <p>Fecha: ____________</p>
+      <p>Terapeuta</p>
+      <p>Contactos (?)</p>
+      <p>Modalidad</p>
+    </div>
+  )
+}
+
+export default TherapistInfo;
