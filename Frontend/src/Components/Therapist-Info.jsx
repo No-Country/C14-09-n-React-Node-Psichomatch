6ee801c0704986d0d696a0548cbf49ed53fd270b
erasmorojastech <erasmorojastech@gmail.com>
2023-10-15 20:52:02 -0300

    Update Components

--- Frontend/src/Components/Therapist-Info.jsx
+++ Frontend/src/Components/Therapist-Info.jsx
@@ -1,10 +1,11 @@
-const TherapistInfo = function () {
+const TherapistInfo = function ({name, lastName, price, cathegory, id, image, rating}) {
   return (
     <div className="w-[65%] bg-blue-400 p-5 rounded-lg">
-      <p>Fecha: ____________</p>
-      <p>Terapeuta</p>
-      <p>Contactos (?)</p>
-      <p>Modalidad</p>
+      <p>{rating} Star</p>
+      <img src={image} alt="Therapist Picture" />
+      <p>Nombre: {name + " "} {lastName}</p>
+      <p>Especialidad: {cathegory}</p>
+      <p>Tarifa: {price}</p>
     </div>
   )
 }
