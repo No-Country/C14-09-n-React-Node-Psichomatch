bdde646b7f8ba5da18f96c53203eea1f3c2a95bf
erasmorojastech <erasmorojastech@gmail.com>
2023-10-15 20:52:55 -0300

    Update Layouts

--- Frontend/src/Layouts/Dashboard-Agenda.jsx
+++ Frontend/src/Layouts/Dashboard-Agenda.jsx
@@ -1,20 +1,9 @@
-import TherapistInfo from "../Components/Therapist-Info"
+import ApointmentInfo from "../Components/Apointment-Info"
 
 const Dashboard_Agenda = function () {
   return (
     <div className="bg-teal-100 p-5 row-start-2 row-end-3 col-start-2 col-end-6 flex flex-col items-center justify-start gap-5">
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
-      <TherapistInfo />
+      <ApointmentInfo />
     </div>
   )
 }
