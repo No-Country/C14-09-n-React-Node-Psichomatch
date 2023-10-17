58586483184f9be44c56bee59809f264f9d9b480
zeduard89 <zeduard89@gmail.com>
2023-10-17 20:50:30 -0300

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Login from './components/Login'
 import Registro from './components/Registro'
 import HomePatient from "./Layouts/Dashboard-Search"
+import Footer from "./Layouts/Footer"
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard"
+
 function App() {
   return (
     <>
-     
      <Routes>
         <Route exact path="/" element={<Registro />} />
         <Route exact path="/home" element={<HomePatient/>} />
       </Routes>
+      <Footer/>
+      <Dashboard/>
     </>
   )
 }
