9666bca155e6ff19b2fc39631adea25763b700e7
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-10-26 11:02:39 -0400

    estilos del historial de citas

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -46,37 +46,50 @@ useEffect(() => {
         <div className="md:mx-6 md:p-12">
         <div className="text-center">
             <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold">
-              Agenda tu cita
+              Panel Principal
             </h1>
-          </div>
-     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
       onClick={() => navigate(`/psicologos/${params.id}`)}
 
      >Elegir a mi terapueta</button>
+          </div>
      <div className="p-12">
      <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold text-center">Historial de Citas</h1>
+       <table className="table-auto">
+           <thead>
+             <tr>
+             <th>Fecha de la cita</th>
+             <th>Hora de asistencia</th>
+            <th>Estatus</th>
+            <th>Terapueta agendado</th>
+            <th>Acción</th>
+            </tr>
+           </thead>
      {data && data.map((reservation, index) => (
-        <div key={index}>
-          <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold text-center">
-            Fecha de la cita: {new Date(reservation.Availability.date).toLocaleDateString()}
-          </h1>
-          <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold text-center">
-            Hora de asistencia: {reservation.Availability.Hour.hour}
-          </h1>
-          <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold text-center">
-            Estatus: {reservation.Availability.status == true ? "Orden Agendada" : "No hay orden agendada"}
-          </h1>
-          <h1 className="mb-12 mt-1 pb-1 text-xl font-semibold text-center">
-            Terapueta agendado: {reservation.Therapist.name} {reservation.Therapist.lastName}
-          </h1>
+             <tbody>
+             <tr>
+          <td>
+           {new Date(reservation.Availability.date).toLocaleDateString()}
+          </td>
+          <td className="px-6 py-4">
+             {reservation.Availability.Hour.hour}
+          </td>
+          <td className="px-6 py-4">
+           {reservation.Availability.status == true ? "Orden Agendada" : "No hay orden agendada"}
+          </td>
+          <td className="px-6 py-4">
+           {reservation.Therapist.name} {reservation.Therapist.lastName}
+          </td>
           <button 
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded"
             onClick={() => deletePatientReservation(reservation.id)}
             >
        Cancelar Cita
        </button>
-        </div>
+       </tr>
+         </tbody>
       ))}
+       </table>
      </div>
      </div>
      </div>
