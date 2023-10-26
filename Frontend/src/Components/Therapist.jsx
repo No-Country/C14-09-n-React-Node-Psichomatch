c2a2d1722d9dd763e3f92434193ba1f31f51d37c
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-10-26 16:45:39 -0400

    cambios

--- Frontend/src/Components/Therapist.jsx
+++ Frontend/src/Components/Therapist.jsx
@@ -1,57 +1,52 @@
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import { GetTherapist } from "../api/therapist_api";
 import { useNavigate, useParams } from 'react-router-dom';
 
 const Therapist = () => {
+  const [therapists, setTherapist] = useState([]);
+  const navigate = useNavigate();
+  const params = useParams();
 
-const [therapists, setTherapist] = useState([])
-const navigate = useNavigate()
-const params = useParams();
+  useEffect(() => {
+    loadTherapist();
+  }, []);
 
-useEffect(() => {
- loadTherapist()
-}, [])
+  const loadTherapist = async () => {
+    const response = await GetTherapist();
+    const data = response.data.therapists;
+    setTherapist(data);
+  };
 
-
-const loadTherapist = async () => {
-   const response = await GetTherapist()
-   const data = response.data.therapists
-   setTherapist(data)
-   
-}
-
-return(
-<div className="flex w-full h-screen items-center justify-center">
-  <div className="w-full flex items-center justify-center lg:w-1/2">
-    <div className="md:mx-6 md:p-12">
-      <div className="text-center">
-        <div className="grid">
-          {therapists.map(therapist => ( 
-            <div className="max-w-sm rounded-lg overflow-hidden shadow-xl m-2" key={therapist.id}>
-              <div className="relative overflow-hidden bg-cover bg-no-repeat">
-                <img className="w-40 h-42 rounded-full" src={therapist.image} alt="Sunset in the mountains" />
-              </div>
-              <div className="px-6 py-4">
-                <div className="flex flex-wrap justify-center mb-4">
-                  <h1>{therapist.name} {therapist.lastName}</h1>
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <div className="md:mx-6 md:p-12">
+          <div className="text-center">
+            {therapists.map(therapist => (
+              <div className="flex items-center mb-4" key={therapist.id}>
+                <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                  <img className="w-40 h-42 rounded-full" src={therapist.image} alt="Sunset in the mountains" />
                 </div>
-                <h1><span className="font-semibold">Precio:</span> ${therapist.price}</h1>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mt-4"
-                 onClick={() => navigate(`/agendarcita/${therapist.id}/${params.id}`)}
-                 >
-                  Agendar Cita
-                </button>
+                <div className="px-6 py-4">
+                  <h1 className="text-gray-900 leading-none font-semibold">{therapist.name} {therapist.lastName}</h1>
+                </div>
+                
+                <div className="px-6 py-4">
+                  <button
+                    className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={() => navigate(`/agendarcita/${therapist.id}/${params.id}`)}
+                  >
+                    Agendar Cita
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-)
-
-}
+  );
+};
 
 export default Therapist;
+
