fec6ec8d97df54adc40cbcf87147b569213770a2
scarletvargas <scarletvf47@gmail.com>
2023-10-27 16:11:00 -0400

    services page completed

--- Frontend/src/Pages/Services.jsx
+++ Frontend/src/Pages/Services.jsx
@@ -10,26 +10,38 @@ import iconGestalt from "../assets/Icons/iconGestalt.svg";
 const services = [
   {
     name: "Cognitivo conductual",
+    description:
+      "Se centra en la identificación y cambio de patrones de pensamiento negativos y comportamientos perjudiciales para promover una mejor salud mental.",
     imageSrc: iconCognitiveBehavioral,
   },
   {
     name: "Interactiva",
+    description:
+      "Destaca la interacción y la comunicación abierta entre el terapeuta y el paciente para abordar los problemas emocionales y mejorar las relaciones.",
     imageSrc: iconInteractive,
   },
   {
     name: "Psicoanálisis",
+    description:
+      "Se basa en explorar el subconsciente para comprender y resolver conflictos emocionales y problemas psicológicos profundos.",
     imageSrc: iconPsychoanalysis,
   },
   {
     name: "Humanista",
+    description:
+      "Se enfoca en el crecimiento personal, la autorrealización y la conexión emocional, con énfasis en el potencial humano.",
     imageSrc: iconHumanist,
   },
   {
     name: "Sistémica",
+    description:
+      "Examina las relaciones y los sistemas familiares para abordar los desafíos personales a través del contexto social y familiar.",
     imageSrc: iconSystemic,
   },
   {
     name: "Gestalt",
+    description:
+      "Pone énfasis en la conciencia del momento presente y la totalidad de la experiencia personal para promover un mayor entendimiento y autenticidad emocional.",
     imageSrc: iconGestalt,
   },
 ];
@@ -39,9 +51,16 @@ export const Services = () => {
     <div className="mx-auto px-2 sm:px-6 pt-16 lg:pt-24 xl:pt-[136px]">
       <div className="flex justify-center flex-wrap gap-x-20 gap-y-12">
         {services.map((service) => (
-          <div key={service.name} className="flex justify-start flex-col">
-            <div className="flex items-center justify-center w-[266px] h-[328px] overflow-hidden rounded-2xl bg-[#F9F6FF]">
-              <img src={service.imageSrc} alt={service.name} className="" />
+          <div key={service.name} className="group relative block">
+            <div className="relative transform transition-transform flex items-center justify-center w-[266px] h-[328px] overflow-hidden rounded-2xl bg-[#F9F6FF]">
+              <div className="transition-opacity group-hover:absolute group-hover:opacity-0">
+                <img src={service.imageSrc} alt={service.name} />
+              </div>
+              <div className="absolute opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 flex items-center justify-center px-8">
+                <p className="font-medium text-lg text-Gray-dark">
+                  {service.description}
+                </p>
+              </div>
             </div>
             <h3 className="text-2xl text-black font-semibold w-[160px] px-[66px] mt-5">
               {service.name}
