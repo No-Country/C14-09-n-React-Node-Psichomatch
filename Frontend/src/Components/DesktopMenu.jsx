416690c4d9c0d79bac71a940e8055c68e6daf237
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-10-29 09:10:37 -0400

    cambios

---
+++ Frontend/src/Components/DesktopMenu.jsx
@@ -0,0 +1,54 @@
+import { NavLink } from "react-router-dom";
+
+function DesktopMenu({ jwt, handleAccountExit }) {
+  return (
+    <>
+
+      {jwt.token !== undefined ? (
+        <>
+         <NavLink
+        to="/servicios"
+        className="text-base text-black font-medium hover:text-[#A9A9A9]"
+      >
+        Mi Calendario
+      </NavLink>
+          <NavLink
+            to={`/psicologos/`}
+            className="text-base text-black font-medium hover:text-[#A9A9A9]"
+          >
+            Encuentra tú Psicólogos
+          </NavLink>
+          <button
+            className="text-white text-base font-medium text-center bg-Gray-dark py-4 px-6 rounded-[32px] w-[214px] hover:bg-[#4f4f4f]"
+            onClick={handleAccountExit}
+          >
+            Cerrar Sesion
+          </button>
+        </>
+      ) : (
+        <>
+         <NavLink
+        to="/servicios"
+        className="text-base text-black font-medium hover:text-[#A9A9A9]"
+         >
+        Servicios
+      </NavLink>
+          <NavLink
+            to="/registro"
+            className="text-base text-black font-medium hover:text-[#A9A9A9]"
+          >
+            Registarme
+          </NavLink>
+          <NavLink
+            to="/login"
+            className="text-white text-base font-medium text-center bg-Gray-dark py-4 px-6 rounded-[32px] w-[214px] hover:bg-[#4f4f4f]"
+          >
+            Iniciar sesión
+          </NavLink>
+        </>
+      )}
+    </>
+  );
+}
+
+export default DesktopMenu;
